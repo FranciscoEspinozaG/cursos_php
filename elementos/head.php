3e82e0298290d3590c7a3fd4bc086d675588321e
Francisco <francisco.espinoza@vasslatam.com>
2023-01-16 22:36:45 -0300

    actualizaciones para dos equipos

--- elementos/head.php
+++ elementos/head.php
@@ -1,6 +1,43 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
+    <script>
+        let schemeColor = window.matchMedia("(prefers-color-scheme: dark)").matches;
+        if(schemeColor){
+            var darkMode = true;
+            document.write('<?php $darkMode = true ?>');
+        }else{
+            var darkMode = false;
+            document.write('<?php $darkMode = false ?>');
+        }
+    </script>
+    <?php
+        if($darkMode){
+            $schemeColor = 'dark';
+        }else{
+            $schemeColor = 'white';
+        }
+
+        echo $schemeColor;
+        echo $darkMode;
+    ?>
+    <link rel="apple-touch-icon" sizes="57x57" href="img/apple-icon-57x57-<?php echo $schemeColor ?>.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="img/apple-icon-60x60-<?php echo $schemeColor ?>.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="img/apple-icon-72x72-<?php echo $schemeColor ?>.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="img/apple-icon-76x76-<?php echo $schemeColor ?>.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="img/apple-icon-114x114-<?php echo $schemeColor ?>.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="img/apple-icon-120x120-<?php echo $schemeColor ?>.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="img/apple-icon-144x144-<?php echo $schemeColor ?>.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="img/apple-icon-152x152-<?php echo $schemeColor ?>.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="img/apple-icon-180x180-<?php echo $schemeColor ?>.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="img/android-icon-192x192-<?php echo $schemeColor ?>.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32-<?php echo $schemeColor ?>.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="img/favicon-96x96-<?php echo $schemeColor ?>.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="img/favicon-16x16-<?php echo $schemeColor ?>.png">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="img/ms-icon-144x144-<?php echo $schemeColor ?>.png">
+    <meta name="theme-color" content="#ffffff">
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
 // call to datatables function
 $(document).ready( function () {
     $('#myTable').DataTable();
+
+    if(window.matchMedia && window.matchMedia('(prefers.color-scheme: dark)').matches){
+        let darkMode = true;
+    }
+
+    // document.cookie =  "darkMode=" + darkMode + "; SameSite=None; Secure";
+    document.cookie = "mode=darkMode; SameSite=None; Secure";
 } );
 
 // custom js
-console.log('funciona')
+// console.log('funciona')
